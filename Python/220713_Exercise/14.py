b6c3d3819a3f16f484a426153708a63163554663
kklee0930 <kklee0930@gmail.com>
2022-07-18 14:50:40 +0900

    TIL python exercise update

---
+++ Python/220713_Exercise/14.py
@@ -0,0 +1,23 @@
+'''문자열 word가 주어질 때, 해당 문자열에서 a 개수를 구하세요.
+count() 메서드 사용 금지'''
+
+word = 'apple'
+count = 0
+for i in word:
+    if i == 'a':
+        count += 1
+print(count)
+
+word_2 = 'banana'
+count = 0
+for i in word_2:
+    if i == 'a':
+        count += 1
+print(count)
+
+word_3 = 'kiwi'
+count = 0
+for i in word_3:
+    if i == 'a':
+        count += 1
+print(count)
